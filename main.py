8bf58739112ddc58c5232114b81b7530c2b91478
almon-s <almon.subba@gmail.com>
2023-01-11 15:30:09 +0800

    Final

--- main.py
+++ main.py
@@ -1,9 +1,101 @@
+import random
+
+board = ["-", "-", "-",
+         "-", "-", "-",
+         "-", "-", "-"]
+
+currentPlayer = "X"
+winner = None
+gameRunning = True
+
 # Printing a gameboard
+def printBoard(board):
+    print("----------")
+    print(board[0] + " | " + board[1] + " | " + board[2])
+    print("----------")
+    print(board[3] + " | " + board[4] + " | " + board[5])
+    print("----------")
+    print(board[6] + " | " + board[7] + " | " + board[8])    
+    print("----------")
 
 # Take player input
+def playerInput(board):
+    inp = int(input("Enter a number between 1-9: "))
+    if inp >= 1 and inp <= 9 and board[inp-1] == "-":
+        board[inp-1] = currentPlayer
+    else:
+        print("A player is already on that spot!")
 
 # Check for win or tie
+def checkRow(board):
+    global winner
+    if board[0] == board[1] == board[2] and board[1] != "-":
+        winner = board[0]
+        return True
+    elif board[3] == board[4] == board[5] and board[3] != "-":
+        winner = board[3]
+        return True
+    elif board[6] == board[7] == board[8] and board[6] != "-":
+        winner = board[6]
+        return True
+
+def checkColumn(board):
+    global winner
+    if board[0] == board[3] == board[6] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[1] == board[4] == board[7] and board[1] != "-":
+        winner = board[1]
+        return True
+    elif board[2] == board[5] == board[8] and board[2] != "-":
+        winner = board[2]
+        return True
+
+def checkDiag(board):
+    global winner
+    if board[0] == board[4] == board[8] and board[0] != "-":
+        winner = board[0]
+        return True
+    elif board[2] == board[4] == board[6] and board[2] != "-":
+        winner = board[2]
+        return True
+
+def checkTie(board):
+    global gameRunning
+    if "-" not in board:
+        printBoard(board)
+        print("It's a tie!")
+        gameRunning = False
+
+def checkWin(board):
+    global gameRunning
+    if checkRow(board) or checkColumn(board) or checkDiag(board):
+        print(f"The winner is {winner}")
+        gameRunning = False
 
 # Switch the player
+def switchPlayer():
+    global currentPlayer
+    if currentPlayer == "X":
+        currentPlayer = "O"
+    else:
+        currentPlayer = "X"
 
-# Check for win or tie again
+# Computer
+def computer(board):
+     while currentPlayer == "O":
+        position = random.randint(0, 8)
+        if board[position] == "-":
+            board[position] = "O"
+            switchPlayer()
+
+
+while gameRunning:
+    printBoard(board)
+    playerInput(board)
+    checkWin(board)
+    checkTie(board)
+    switchPlayer()
+    computer(board)
+    checkWin(board)
+    checkTie(board)
